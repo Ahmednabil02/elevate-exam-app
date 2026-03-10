038d553e5301f7e4b7b3dbee20b575aca7575375
gaser014 <gaser.mohamed158@gmail.com>
2026-03-10 23:39:17 +0200

    refactor routing and add validation utilities

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,21 +1,15 @@
+import 'package:exam_app/core/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRoutes {
-  static const String home = '/';
-  static const String login = '/login';
-  static const String signUp = '/signUp';
-  static const String forgotPassword = '/forgotPassword';
-  static const String verifyCode = '/verifyCode';
-  static const String resetPassword = '/resetPassword';
-  static const String profile = '/profile';
-  static const String exams = '/exams';
+
 
   static final GoRouter router = GoRouter(
-    initialLocation: home,
+    initialLocation: Routes.home,
     routes: [
       GoRoute(
-        path: home,
+        path: Routes.home,
         builder: (BuildContext context, GoRouterState state) {
           return const Scaffold(
             body: Center(
