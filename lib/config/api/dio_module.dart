4f77a3a71eba94c3db1b8970ace47fa7e57bd9e9
gaserMohaed <elamahwy@std.mans.edu.ed>
2026-03-10 23:35:56 +0200

    add Dio module with secure storage and error handling

---
+++ lib/config/api/dio_module.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:injectable/injectable.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+import 'app_interceptor.dart';
+import 'end_points.dart';
+
+@module
+abstract class DioModule {
+  @lazySingleton
+  FlutterSecureStorage secureStorage() => const FlutterSecureStorage();
+
+  @singleton
+  Dio dio() {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: EndPoints.baseUrl,
+        sendTimeout: Duration(seconds: 45),
+        connectTimeout: Duration(seconds: 45),
+      ),
+    );
+    dio.interceptors.add(AppInterceptors(dio: dio, fss: secureStorage()));
+    dio.interceptors.addAll([
+      if (kDebugMode)
+        PrettyDioLogger(
+          request: true,
+          requestBody: true,
+          responseHeader: true,
+          responseBody: true,
+          error: true,
+          compact: true,
+          requestHeader: true,
+        ),
+    ]);
+    return dio;
+  }
+
+  @lazySingleton
+  CancelToken cancelToken() => CancelToken();
+
+  @lazySingleton
+  InternetConnection internetConnection() => InternetConnection();
+}
