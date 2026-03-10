ccc4c43a6249c281fbde0033aef2fd6b25c3638a
gaser014 <gaser.mohamed158@gmail.com>
2026-03-10 23:52:16 +0200

    add AppAssets class for centralized asset management

---
+++ lib/core/values/app_assets.dart
@@ -0,0 +1,44 @@
+class AppAssets {
+  AppAssets._();
+
+  static const String add = "assets/icons/add.svg";
+  static const String address = "assets/icons/address.svg";
+  static const String alarmClock = "assets/icons/alarm_clock.svg";
+  static const String arrowBackIos = "assets/icons/arrow_back_ios.svg";
+  static const String carbonResultDraft =
+      "assets/icons/carbon_result_draft.svg";
+  static const String category = "assets/icons/category.svg";
+  static const String check = "assets/icons/check.svg";
+  static const String delete = "assets/icons/delete.svg";
+  static const String diamond = "assets/icons/diamond.svg";
+  static const String error = "assets/icons/error.svg";
+  static const String gift = "assets/icons/gift.svg";
+  static const String heartEyes = "assets/icons/heart_eyes.svg";
+  static const String home = "assets/icons/home.svg";
+  static const String homeFilled = "assets/icons/home_filled.svg";
+  static const String location = "assets/icons/location.svg";
+  static const String locationOn = "assets/icons/location_on.svg";
+  static const String logout = "assets/icons/logout.svg";
+  static const String person = "assets/icons/person.svg";
+  static const String personFilled = "assets/icons/person_filled.svg";
+  static const String photoCamera = "assets/icons/photo_camera.svg";
+  static const String remove = "assets/icons/remove.svg";
+  static const String search = "assets/icons/search.svg";
+  static const String shoppingCart = "assets/icons/shopping_cart.svg";
+  static const String sort = "assets/icons/sort.svg";
+  static const String switchIcon = "assets/icons/switch.svg";
+  static const String translate = "assets/icons/translate.svg";
+  static const String tulip = "assets/icons/tulip.svg";
+  static const String tune = "assets/icons/tune.svg";
+  static const String visibilityOff = "assets/icons/visibility_off.svg";
+  
+  static const String colorPalette = "assets/images/color_palette.png";
+  static const String draftingTools = "assets/images/drafting_tools.png";
+  static const String image1 = "assets/images/image_1.png";
+  static const String image2 = "assets/images/image_2.png";
+  static const String languageTranslator =
+      "assets/images/language_translator.png";
+  static const String microscope = "assets/images/microscope.png";
+  static const String profit = "assets/images/profit.png";
+  static const String sandClock1 = "assets/images/sand_clock_1.png";
+}
