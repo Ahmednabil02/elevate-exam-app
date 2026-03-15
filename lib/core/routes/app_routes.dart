661ff210bd25dcd589da65572d27da381b70f6c0
Ahmednabil02 <AhmedNabil72@outlook.com>
2026-03-15 13:28:07 +0200

    update demo

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:exam_app/config/dependency_injection/di.dart';
 import 'package:exam_app/core/routes/routes.dart';
 import 'package:exam_app/feature/forget_password/presentation/screen/forget_password_page.dart';
+import 'package:exam_app/feature/home/presentation/screen/home_page.dart';
 import 'package:exam_app/feature/login/presentation/cubit/login_cubit.dart';
 import 'package:exam_app/feature/login/presentation/screen/login_page.dart';
 import 'package:exam_app/feature/sign_up/presentation/screen/sign_up_page.dart';
@@ -8,10 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
-
 class AppRoutes {
-
-
   static final GoRouter router = GoRouter(
     initialLocation: Routes.login,
     routes: [
@@ -27,11 +25,7 @@ class AppRoutes {
       GoRoute(
         path: Routes.home,
         builder: (BuildContext context, GoRouterState state) {
-          return const Scaffold(
-            body: Center(
-              child: Text('Core & Config Baseline Ready'),
-            ),
-          );
+          return HomePage();
         },
       ),
       GoRoute(
@@ -43,8 +37,7 @@ class AppRoutes {
       GoRoute(
         path: Routes.register,
         builder: (BuildContext context, GoRouterState state) {
-          return const SignUpPage(
-          );
+          return const SignUpPage();
         },
       ),
     ],
--- lib/feature/login/presentation/screen/login_page.dart
+++ lib/feature/login/presentation/screen/login_page.dart
@@ -163,7 +163,7 @@ class _LoginPageState extends State<LoginPage> {
       initial: () {},
       loading: () {},
       success: (data) {
-        context.pushReplacementNamed(Routes.home);
+        context.go(Routes.home);
       },
       error: (exception) {
         ScaffoldMessenger.of(context).showSnackBar(
