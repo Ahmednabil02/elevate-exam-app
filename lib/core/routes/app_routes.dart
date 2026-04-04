713c334ee5ff5e5b4c0f1304b5962fa8acf9a08d
gaser014 <gaser.mohamed158@gmail.com>
2026-04-04 20:41:52 +0200

    feat: add exam details and questions pages, enhance exam card with new components and routing

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,11 +1,14 @@
 import 'package:exam_app/config/api/end_points.dart';
 import 'package:exam_app/config/dependency_injection/di.dart';
 import 'package:exam_app/core/routes/routes.dart';
+import 'package:exam_app/feature/exams/domain/entities/exam_entity.dart';
+import 'package:exam_app/feature/exams/presentation/screen/exam_details_page.dart';
 import 'package:exam_app/feature/exams/presentation/screen/exams_page.dart';
 import 'package:exam_app/feature/forget_password/presentation/screen/forget_password_page.dart';
 import 'package:exam_app/feature/home/presentation/screen/home_page.dart';
 import 'package:exam_app/feature/login/presentation/cubit/login_cubit.dart';
 import 'package:exam_app/feature/login/presentation/screen/login_page.dart';
+import 'package:exam_app/feature/questions/presentation/screen/question_page.dart';
 import 'package:exam_app/feature/sign_up/presentation/screen/sign_up_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -38,6 +41,26 @@ class AppRoutes {
         name: Routes.exams,
         builder: (BuildContext context, GoRouterState state) {
           return ExamsPage();
+        },
+      ),
+      GoRoute(
+        path: Routes.examDetails,
+        name: Routes.examDetails,
+        builder: (BuildContext context, GoRouterState state) {
+          final Map<String, dynamic> extra =
+              state.extra as Map<String, dynamic>;
+          final ExamEntity exam = extra['exam'];
+          return ExamDetailsPage(exam: exam);
+        },
+      ),
+      GoRoute(
+        path: Routes.questions,
+        name: Routes.questions,
+        builder: (BuildContext context, GoRouterState state) {
+          final Map<String, dynamic> extra =
+              state.extra as Map<String, dynamic>;
+          final ExamEntity exam = extra['exam'];
+          return QuestionPage(exam: exam);
         },
       ),
       GoRoute(
