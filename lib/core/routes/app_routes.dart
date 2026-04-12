0760fc0d52d949bd5dd1401d154a3bde04b6fe57
gaser014 <gaser.mohamed158@gmail.com>
2026-04-12 14:13:25 +0200

    feat: remove login, sign up, and forget password features from app routes and DI config

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -4,14 +4,14 @@ import 'package:exam_app/core/routes/routes.dart';
 import 'package:exam_app/feature/exams/domain/entities/exam_entity.dart';
 import 'package:exam_app/feature/exams/presentation/screen/exam_details_page.dart';
 import 'package:exam_app/feature/exams/presentation/screen/exams_page.dart';
-import 'package:exam_app/feature/forget_password/presentation/screen/forget_password_page.dart';
-import 'package:exam_app/feature/home/presentation/screen/home_page.dart';
-import 'package:exam_app/feature/login/presentation/cubit/login_cubit.dart';
-import 'package:exam_app/feature/login/presentation/screen/login_page.dart';
+// import 'package:exam_app/feature/forget_password/presentation/screen/forget_password_page.dart';
+// import 'package:exam_app/feature/home/presentation/screen/home_page.dart';
+// import 'package:exam_app/feature/login/presentation/cubit/login_cubit.dart';
+// import 'package:exam_app/feature/login/presentation/screen/login_page.dart';
 import 'package:exam_app/feature/questions/presentation/screen/question_page.dart';
-import 'package:exam_app/feature/sign_up/presentation/screen/sign_up_page.dart';
+// import 'package:exam_app/feature/sign_up/presentation/screen/sign_up_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:go_router/go_router.dart';
 
@@ -19,23 +19,23 @@ class AppRoutes {
   static final GoRouter router = GoRouter(
     initialLocation: Routes.login,
     routes: [
-      GoRoute(
-        path: Routes.login,
-        name: Routes.login,
-        builder: (BuildContext context, GoRouterState state) {
-          return BlocProvider(
-            create: (context) => getIt<LoginCubit>(),
-            child: const LoginPage(),
-          );
-        },
-      ),
-      GoRoute(
-        path: Routes.home,
-        name: Routes.home,
-        builder: (BuildContext context, GoRouterState state) {
-          return HomePage();
-        },
-      ),
+      // GoRoute(
+      //   path: Routes.login,
+      //   name: Routes.login,
+      //   builder: (BuildContext context, GoRouterState state) {
+      //     return BlocProvider(
+      //       create: (context) => getIt<LoginCubit>(),
+      //       child: const LoginPage(),
+      //     );
+      //   },
+      // ),
+      // GoRoute(
+      //   path: Routes.home,
+      //   name: Routes.home,
+      //   builder: (BuildContext context, GoRouterState state) {
+      //     return HomePage();
+      //   },
+      // ),
       GoRoute(
         path: Routes.exams,
         name: Routes.exams,
@@ -63,20 +63,20 @@ class AppRoutes {
           return QuestionPage(exam: exam);
         },
       ),
-      GoRoute(
-        path: Routes.forgetPassword,
-        name: Routes.forgetPassword,
-        builder: (BuildContext context, GoRouterState state) {
-          return ForgetPasswordPage();
-        },
-      ),
-      GoRoute(
-        path: Routes.register,
-        name: Routes.register,
-        builder: (BuildContext context, GoRouterState state) {
-          return const SignUpPage();
-        },
-      ),
+      // GoRoute(
+      //   path: Routes.forgetPassword,
+      //   name: Routes.forgetPassword,
+      //   builder: (BuildContext context, GoRouterState state) {
+      //     return ForgetPasswordPage();
+      //   },
+      // ),
+      // GoRoute(
+      //   path: Routes.register,
+      //   name: Routes.register,
+      //   builder: (BuildContext context, GoRouterState state) {
+      //     return const SignUpPage();
+      //   },
+      // ),
     ],
     redirect: (context, state) async {
       final token = await getIt<FlutterSecureStorage>().read(
